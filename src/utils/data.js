fa43d110e9b350a0d430b00f3dd1278a0999069e
AnnisaFg <annisadarwis123@gmail.com>
2023-03-27 22:43:56 +0800

    setup

---
+++ src/utils/data.js
@@ -0,0 +1,54 @@
+import I1 from "../img/i1.png";
+import F1 from "../img/f1.png";
+import C3 from "../img/c3.png";
+import Fi1 from "../img/fi1.png";
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Teh Tarik",
+    decp: "Teh Tarik Panas",
+    price: "25",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Cokelat Panas",
+    decp: "Cokelat Panas",
+    price: "10",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Kopiriolo",
+    decp: "Kopiriolo",
+    price: "8",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Kentang Goreng",
+    decp: "Kentang Goreng",
+    price: "20",
+    imageSrc: Fi1,
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Makanan",
+    urlParamName: "makanan",
+  },
+  {
+    id: 2,
+    name: "Cemilan",
+    urlParamName: "cemilan",
+  },
+
+  {
+    id: 3,
+    name: "Minuman",
+    urlParamName: "drink",
+  },
+];
