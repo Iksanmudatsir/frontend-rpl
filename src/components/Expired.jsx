fd85e52a366967af0ef41c898945a352066cc42e
A. Ichsan Mudatsir <iksan0049@gmail.com>
2023-06-02 13:14:52 +0800

    update

--- src/components/Expired.jsx
+++ src/components/Expired.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import EmptyCart from "../img/emptyCart.svg";
 
 function Expired() {
   return (
-    <div>
-      <h2>Silahkan scan barcode lagi</h2>
-    </div>
+    <>
+    <div className="absolute inset-0 h-full w-full object-cover bg-black/50"></div>
+      <div 
+        className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4 bg-[#D9D9D9]/80 py-10 object-cover bg-no-repeat rounded-lg"
+        style={{ backgroundImage: `url(${EmptyCart})`, backgroundPosition: 'bottom'}}>
+      <div className="px-3 mx-auto mx-16">
+      <span><strong>Silahkan scan Barcode lagi</strong></span>
+      </div>
+     </div>
+     </>
   );
 }
 
